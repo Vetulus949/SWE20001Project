1d90d3cbd9191efe53ce1642acf8fc541355361b
Dom <34756785+Vetulus949@users.noreply.github.com>
2023-10-02 13:01:35 +1100

    App funcitonality
    finished admin page
    added and finish tech and general pages
    also added a signout button

--- App/app.js
+++ App/app.js
@@ -33,36 +33,58 @@ router.post("/signin", (req, res) => {
     var pass = req.body.pass;
     console.log("signin attempt:\n\t"+name+"\n\t"+pass);
     var sql = "SELECT * FROM users WHERE name = \""+name+"\";";
-    await  = con.query(sql, (err, thisUser) => {
+    con.query(sql, (err, thisUser) => {
         if (err) throw err;
-        if (thisUser[0].pass == pass)
+        if (thisUser[0])
         {
-            console.log("signin successful");
-            switch(thisUser[0].permissions)
+            if (thisUser[0].pass == pass)
             {
-                case 0:
-                    // general user
-                    break;
-                case 1:
-                    // tech
-                    break;
-                case 2:
-                    // admin
-                    // get all users
-                    sql = "SELECT * FROM users;"
-                    con.query(sql, (err, resultUsers) => {
-                        if (err) throw err;
-                        users = resultUsers;
-                        // get all tickets
-                        sql = "SELECT * FROM tickets;"
-                        con.query(sql, (err, resultTickets) => {
+                console.log("signin successful");
+                switch(thisUser[0].permissions)
+                {
+                    case 0:
+                        // general user
+                        res.render("general", {message:"hello, "+thisUser[0].name, userid:thisUser[0].userID})
+                        break;
+                    case 1:
+                        // tech
+                        sql = "SELECT * FROM tickets WHERE status <> 2 AND (tech <> "+thisUser[0].userID+" OR ISNULL(tech));"
+                        con.query(sql, (err, resultsNotClaimed) => {
                             if (err) throw err;
-                            tickets = resultTickets;
-                            // render new page
-                            res.render("admin", {message:"hello, "+thisUser[0].name, userList:resultUsers, ticketList:resultTickets});
-                        }); 
-                    });
-                break;
+                            notClaimed = resultsNotClaimed;
+                            sql = "SELECT * FROM tickets WHERE status <> 2 AND tech = "+thisUser[0].userID+";"
+                            con.query(sql, (err, resultsClaimed) => {
+                                if (err) throw err;
+                                claimed = resultsClaimed;
+                                res.render("technician", {message: "hello, "+thisUser[0].name, userid:thisUser[0].userID, 
+                                    notClaimed:resultsNotClaimed, claimed:claimed});
+                            });
+                        });
+                        break;
+                    case 2:
+                        // admin
+                        // get all users
+                        sql = "SELECT * FROM users;"
+                        con.query(sql, (err, resultUsers) => {
+                            if (err) throw err;
+                            users = resultUsers;
+                            // get all tickets
+                            sql = "SELECT * FROM tickets ORDER BY status;"
+                            con.query(sql, (err, resultTickets) => {
+                                if (err) throw err;
+                                tickets = resultTickets;
+                                // render new page
+                                res.render("admin", {message:"hello, "+thisUser[0].name, userid:thisUser[0].userID, userList:resultUsers, ticketList:resultTickets});
+                            }); 
+                        });
+                    break;
+                }
+            }
+            else
+            {
+                console.log("signin unsuccessful");
+                // render page
+                res.render("signin", {message:"Signin unsuccessful.", });
             }
         }
         else
@@ -73,11 +95,16 @@ router.post("/signin", (req, res) => {
         }
     });
 });
+// signout
+router.post("/signout", (req, res) => {
+    res.render("signin", {message:"Signed out."});
+});
 // ######################################
 //      Admin Stuff
 // ######################################
 // create user
 router.post("/createUser", (req, res) => {
+    var thisUser = req.query.thisuser;
     var name = req.body.name;
     var pass = req.body.pass;
     var perms = req.body.perms;
@@ -90,12 +117,12 @@ router.post("/createUser", (req, res) => {
             if (err) throw err;
             users = resultUsers;
             // get all tickets
-            sql = "SELECT * FROM tickets;"
+            sql = "SELECT * FROM tickets ORDER BY status;"
             con.query(sql, (err, resultTickets) => {
                 if (err) throw err;
                 tickets = resultTickets;
                 // render new page
-                res.render("admin", {message:"new user successfully created", userList:resultUsers, ticketList:resultTickets});
+                res.render("admin", {message:"new user successfully created", userid:thisUser, userList:resultUsers, ticketList:resultTickets});
             }); 
         });
     });
@@ -104,6 +131,7 @@ router.post("/createUser", (req, res) => {
 router.post("/editUser", (req, res) => {
     // get id
     var id = req.query.id;
+    var thisuser = req.query.thisuser;
     // are we deleting?
     if (req.body.delete)
     {
@@ -116,12 +144,12 @@ router.post("/editUser", (req, res) => {
                 if (err) throw err;
                 users = resultUsers;
                 // get all tickets
-                sql = "SELECT * FROM tickets;"
+                sql = "SELECT * FROM tickets ORDER BY status;"
                 con.query(sql, (err, resultTickets) => {
                     if (err) throw err;
                     tickets = resultTickets;
                     // render new page
-                    res.render("admin", {message:"new user successfully created", userList:resultUsers, ticketList:resultTickets});
+                    res.render("admin", {message:"userID "+id+" successfully deleted", userid:thisuser, userList:resultUsers, ticketList:resultTickets});
                 }); 
             });
         });
@@ -146,12 +174,12 @@ router.post("/editUser", (req, res) => {
                 if (err) throw err;
                 users = resultUsers;
                 // get all tickets
-                sql = "SELECT * FROM tickets;"
+                sql = "SELECT * FROM tickets ORDER BY status;"
                 con.query(sql, (err, resultTickets) => {
                     if (err) throw err;
                     tickets = resultTickets;
                     // render new page
-                    res.render("admin", {message:"new users successfully created", userList:resultUsers, ticketList:resultTickets});
+                    res.render("admin", {message:"new users successfully created", userid:thisuser, userList:resultUsers, ticketList:resultTickets});
                 }); 
             });
         });
@@ -162,9 +190,115 @@ router.post("/editTicket", (req, res) => {
 
 });
 // ######################################
+//      General User stuff
 // ######################################
-
+// create new ticket
+router.post("/createTicket", (req, res) => {
+    var ticketName = req.body.ticketName;
+    var ticketDesc = req.body.ticketDesc;
+    var creatorID = req.query.id;
+    var sql = "INSERT INTO tickets (name, description, creator)" +
+    "VALUES ('"+ticketName+"', '"+ticketDesc+"', "+creatorID+");";
+    con.query(sql, (err, result) =>
+    {
+        if (err) throw err;
+        console.log("ticket successfully created");
+        res.render("general", {message:"new ticket successfully created", userid:creatorID})
+    })
+})
 // ######################################
+//      Technician Stuff
+// ######################################
+// claiming a ticket
+router.post("/claimTicket", (req, res) => {
+    var ticketID = req.query.ticketID;
+    var techID = req.query.thisuser;
+    var sql = "UPDATE tickets SET tech = "+techID+", status = 1 WHERE ticketID = "+ticketID+";";
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("ticket successfully claimed")
+        sql = "SELECT * FROM tickets WHERE status <> 2 AND (tech <> "+techID+" OR ISNULL(tech));"
+        con.query(sql, (err, resultsNotClaimed) => {
+            if (err) throw err;
+            notClaimed = resultsNotClaimed;
+            sql = "SELECT * FROM tickets WHERE status <> 2 AND tech = "+techID+";";
+            con.query(sql, (err, resultsClaimed) => {
+                if (err) throw err;
+                claimed = resultsClaimed;
+                res.render("technician", {message:"ticket successfully claimed", userid:techID, 
+                    notClaimed:resultsNotClaimed, claimed:claimed});
+            });
+        });
+    });
+});
+// change a ticket
+router.post("/techTicketChanges", (req, res) => {
+    //forfeit
+    var ticketID = req.query.ticketID;
+    var techID = req.query.thisuser;
+    if (req.body.forfeit)
+    {
+        var sql = "UPDATE tickets SET tech = NULL, status = 0 WHERE ticketID = "+ticketID+";";
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log("Ticket successfully forfeit");
+            sql = "SELECT * FROM tickets WHERE status <> 2 AND (tech <> "+techID+" OR ISNULL(tech));"
+            con.query(sql, (err, resultsNotClaimed) => {
+                if (err) throw err;
+                notClaimed = resultsNotClaimed;
+                sql = "SELECT * FROM tickets WHERE status <> 2 AND tech = "+techID+";";
+                con.query(sql, (err, resultsClaimed) => {
+                    if (err) throw err;
+                    claimed = resultsClaimed;
+                    res.render("technician", {message:"ticket successfully claimed", userid:techID, 
+                    notClaimed:resultsNotClaimed, claimed:claimed});
+                });
+            });
+        });
+    } 
+    //commit
+    else if (req.body.commit)
+    {
+        var sql = "UPDATE tickets SET comments = '"+req.body.comments+"' WHERE ticketID = "+ticketID+";";
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log("Ticket comments successfully committed");
+            sql = "SELECT * FROM tickets WHERE status <> 2 AND (tech <> "+techID+" OR ISNULL(tech));"
+            con.query(sql, (err, resultsNotClaimed) => {
+                if (err) throw err;
+                notClaimed = resultsNotClaimed;
+                sql = "SELECT * FROM tickets WHERE status <> 2 AND tech = "+techID+";";
+                con.query(sql, (err, resultsClaimed) => {
+                    if (err) throw err;
+                    claimed = resultsClaimed;
+                    res.render("technician", {message:"ticket successfully claimed", userid:techID, 
+                    notClaimed:resultsNotClaimed, claimed:claimed});
+                });
+            });
+        });
+    }
+    //close
+    else if (req.body.close)
+    {
+        var sql = "UPDATE tickets SET tech = NULL, status = 2 WHERE ticketID = "+ticketID+";";
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log("Ticket successfully closed");
+            sql = "SELECT * FROM tickets WHERE status <> 2 AND (tech <> "+techID+" OR ISNULL(tech));"
+            con.query(sql, (err, resultsNotClaimed) => {
+                if (err) throw err;
+                notClaimed = resultsNotClaimed;
+                sql = "SELECT * FROM tickets WHERE status <> 2 AND tech = "+techID+";";
+                con.query(sql, (err, resultsClaimed) => {
+                    if (err) throw err;
+                    claimed = resultsClaimed;
+                    res.render("technician", {message:"ticket successfully claimed", userid:techID, 
+                    notClaimed:resultsNotClaimed, claimed:claimed});
+                });
+            });
+        });
+    }
+});
 // ######################################
 // start server
 app.use("/", router);
